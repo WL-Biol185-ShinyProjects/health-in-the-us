d7960f83a1c0c1ac4a33a4264395b51015dbc4c5
Elaina Bellone <ebellone@mail.wlu.edu>
2026-02-18 16:43:21 -0500

    code for graph by state that has bars for each category, height is total dollars. Will need to set up a drop down

--- server.R
+++ server.R
@@ -3,14 +3,23 @@ library(shiny)
 # Define server logic required to draw a histogram
 function(input, output) {
   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  output$BarByState <- renderPlot({
+    outputstates %>%
+filter(State == "California") %>%   # change to your state
+group_by(Category) %>%
+summarise(total_dollars = sum(Dollars, na.rm = TRUE)) %>%
+ggplot(aes(x = reorder(Category, total_dollars),
+y = total_dollars,
+fill = Category)) +   # <-- different color per category
+geom_col() +
+labs(
+title = "Total Dollars Spent by Category (California)",
+x = "Category",
+y = "Total Dollars"
+) +
+theme_minimal() +
+theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+theme(legend.position = "none")   # hides legend since colors = categories
     
   })
   
--- ui.R
+++ ui.R
@@ -4,21 +4,16 @@ library(shiny)
 fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("outputstates.csv"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      plotOutput("BarByState")
     )
   )
 )
